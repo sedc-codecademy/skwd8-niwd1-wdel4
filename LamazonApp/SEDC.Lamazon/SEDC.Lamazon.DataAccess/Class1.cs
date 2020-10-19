463fdc4ed2fe44e61ef6c23fb07f4b0485743874
Martin <panovski62@gmail.com>
2020-10-19 21:13:10 +0200

    Class 01 entities diagram and application initial structure added

---
+++ LamazonApp/SEDC.Lamazon/SEDC.Lamazon.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SEDC.Lamazon.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ LamazonApp/SEDC.Lamazon/SEDC.Lamazon.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SEDC.Lamazon.Services
+{
+    public class Class1
+    {
+    }
+}
